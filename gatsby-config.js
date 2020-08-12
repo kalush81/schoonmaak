d318bb7c9371fef137dd8d5e57840d74b9d22999
kalush81 <krzysiek.kaluza@gmail.com>
2020-08-12 14:52:30 +0200

    Install remove-trailing-slashes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
+    //`gatsby-plugin-remove-trailing-slashes`,
     `gatsby-plugin-sass`,
     {
       resolve: "gatsby-plugin-web-font-loader",
