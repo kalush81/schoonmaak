ddb4eb7fd4fb534694e78807a31bfd3e7697f866
kalush81 <krzysiek.kaluza@gmail.com>
2020-08-12 16:25:13 +0200

    require image before loading

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import headerStyle from './header.module.scss'
 export default function Header({page, title}) {
     return (
         <figure className='full-width-wrap nomargin'>
-            <img src={`images/${page}.jpg`} alt={page}/>
+            <img src={require(`../../static/images/${page}.jpg`)} alt={page}/>
             <figcaption className={headerStyle.figcap}>{title}</figcaption>
         </figure>
     )
