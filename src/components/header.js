19ef0aae6f790ed78e3036eb8af5146c6a202272
kalush81 <krzysiek.kaluza@gmail.com>
2020-08-23 13:09:29 +0200

    Implement React awesome slider and customize it

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,71 @@
-import React from 'react'
-import headerStyle from './header.module.scss'
+import React from "react"
+import headerStyle from "./header.module.scss"
+import withAutoplay from "react-awesome-slider/dist/autoplay"
+//import CaptionedHoc from "react-awesome-slider/src/hoc/captioned-images/hoc"
+import AwesomeSlider from "react-awesome-slider"
+import "react-awesome-slider/dist/styles.css"
+import "react-awesome-slider/dist/captioned.css"
+import sliderStyles from "./slider.module.scss"
 
-export default function Header({page, title}) {
+
+const style = {
+    fontWeight: 'bold', 
+    padding: '10px 20px', 
+    marginBottom: 0, 
+    background: 'rgba(255,255,255, 0.6)'
+}
+
+const AutoplaySlider = withAutoplay(AwesomeSlider)
+export default function Header({ page, title }) {
+  if (page === "home") {
     return (
-        <figure className='full-width-wrap nomargin'>
-            <img src={require(`../../static/images/${page}.jpg`)} alt={page}/>
-            <figcaption className={headerStyle.figcap}>{title}</figcaption>
-        </figure>
+      <AutoplaySlider play={false} interval={4000}>
+        <div
+          //className={sliderStyles.container}
+          title="Duurzaamheid_Schoonmaak"
+          data-src={require("../../static/images/slider-pics/Duurzaamheid_Schoonmaak.jpg")}
+        >
+          <div data-type="caption">
+            <h2 style={style}>ONS VISIE</h2>
+            <p>
+              Bij ons werk vormt duurzaamheid een belangrijke pijler. We hebben
+              oog voor het milieu en gaan voor een samenwerking op de lange
+              termijn.
+            </p>
+          </div>
+        </div>
+        <div
+          title="Corona_Virus_Schoonmaak"
+          data-src={require("../../static/images/slider-pics/Corona_Virus_Schoonmaak.jpg")}
+        >
+          <div data-type="caption">
+            <h2 style={style}>INNOVATIEF DESINFECTEREN</h2>
+            <p>
+              Ozon binnenshuis is een effectieve en bewezen methode voor het
+              verwijderen van ziektekiemen, bacteriën en allergenen en virussen.
+            </p>
+          </div>
+        </div>
+        <div
+          title="Milieu_Vriendelijk_Schoonmaak"
+          data-src={require("../../static/images/slider-pics/Milieu_Vriendelijk_Schoonmaak.jpg")}
+        >
+          <div data-type="caption">
+            <h2 style={style}>ONS MISSIE</h2>
+            <p>
+              Schoonmaak Loont focust op kwaliteit en zorgt ervoor dat geniet u
+              van een schoon en representatief resultaat, zonder dat u ergens
+              omkijken naar heeft.
+            </p>
+          </div>
+        </div>
+      </AutoplaySlider>
     )
+  }
+  return (
+    <figure className="full-width-wrap nomargin">
+      <img src={require(`../../static/images/${page}.jpg`)} alt={page} />
+      <figcaption className={headerStyle.figcap}>{title}</figcaption>
+    </figure>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Layout from "../components/layout"
 import Header from "../components/header"
 import Article from "../components/article"
 import Section from "../components/section"
+import headerStyle from '../components/header.module.scss'
 
 export default function Home() {
   return (
     <Layout>
       <Header page="home" title="Wij dragen bij aan het Milieu." />
+      <figcaption className={headerStyle.figcap}>Wij dragen bij aan het Milieu.</figcaption>
       <div className='article-text-container'>
         <Article tekst="wat u zoekt" textSize="uppercase" />
       </div>
