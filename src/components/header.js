8d59e62077ac4a070a2a4f8d226a38f8c64c8142
kalush81 <krzysiek.kaluza@gmail.com>
2020-08-24 09:17:10 +0200

    Style slider

--- src/components/header.js
+++ src/components/header.js
@@ -6,44 +6,47 @@ import "react-awesome-slider/dist/styles.css"
 import "react-awesome-slider/dist/captioned.css"
 import sliderStyles from "./slider.module.scss"
 
-
 const style = {
   container: {
-    border: '10px solid black',
+    border: "10px solid black",
   },
-  imageContainer: {
-    height: '100%'
-  },
+  imageContainer: {},
+
   header: {
-    fontWeight: 'bold', 
-    padding: '10px 20px', 
-    marginBottom: 0, 
-    background: 'rgba(255,255,255, 0.6)'
+    fontWeight: "bold",
+    textTransform: 'uppercase',
+    //padding: "10px 20px",
+    //marginBottom: 0,
   },
   para: {
-    padding: '10px 20px', 
-    marginBottom: 0, 
-    background: 'rgba(255,255,255, 0.6)'
-  }
+    padding: "10px 20px",
+    marginBottom: 0,
+    //background: 'rgba(255,255,255, 0.6)'
+  },
 }
 
 const AutoplaySlider = withAutoplay(AwesomeSlider)
 export default function Header({ page, title }) {
   if (page === "home") {
     return (
-      <AutoplaySlider play={false} interval={4000} className={sliderStyles.container}>
+      <AutoplaySlider
+        play={false}
+        interval={4000}
+        className={sliderStyles.container}
+      >
         <div
-          style={style.imageContainer}
           title="Duurzaamheid_Schoonmaak"
           data-src={require("../../static/images/slider-pics/Duurzaamheid_Schoonmaak.jpg")}
         >
           <div data-type="caption">
-            <h2 style={style.header}>ONS VISIE</h2>
-            <p style={style.para}>
-              Bij ons werk vormt duurzaamheid een belangrijke pijler. We hebben
-              oog voor het milieu en gaan voor een samenwerking op de lange
-              termijn.
-            </p>
+            {/* <div className="container"> */}
+              <h2 style={style.header}>ons visie</h2>
+              <p>
+                Bij ons werk vormt duurzaamheid een belangrijke pijler. We
+                hebben oog voor het milieu en gaan voor een samenwerking op de
+                lange termijn.
+              </p>
+            {/* </div> */}
           </div>
         </div>
         <div
@@ -51,7 +54,7 @@ export default function Header({ page, title }) {
           data-src={require("../../static/images/slider-pics/Corona_Virus_Schoonmaak.jpg")}
         >
           <div data-type="caption">
-            <h2 style={style}>INNOVATIEF DESINFECTEREN</h2>
+            <h2 style={style.header}>INNOVATIEF DESINFECTEREN</h2>
             <p>
               Ozon binnenshuis is een effectieve en bewezen methode voor het
               verwijderen van ziektekiemen, bacteriën en allergenen en virussen.
@@ -63,7 +66,7 @@ export default function Header({ page, title }) {
           data-src={require("../../static/images/slider-pics/Milieu_Vriendelijk_Schoonmaak.jpg")}
         >
           <div data-type="caption">
-            <h2 style={style}>ONS MISSIE</h2>
+            <h2 style={style.header}>ONS MISSIE</h2>
             <p>
               Schoonmaak Loont focust op kwaliteit en zorgt ervoor dat geniet u
               van een schoon en representatief resultaat, zonder dat u ergens
