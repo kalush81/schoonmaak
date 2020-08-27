ef4f84a33c2ede593295188365f481d1921532c0
kalush81 <krzysiek.kaluza@gmail.com>
2020-08-27 15:42:13 +0200

    Setup form zakelijk

--- src/components/form-particulier.js
+++ src/components/form-particulier.js
@@ -10,7 +10,7 @@ const initialFormState = {
   email: "",
   phonenumber: "",
   message: "",
-  gender: "Mevrouw",
+  gender: "Heer",
 }
 
 export default function FormParticulier() {
@@ -31,7 +31,7 @@ export default function FormParticulier() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "fancy-contact", ...formState }),
+      body: encode({ "form-name": "fancy-contact-particulier", ...formState }),
     })
       .then(() => {
         console.log("sent from fancy contact: ", formState)
@@ -44,7 +44,7 @@ export default function FormParticulier() {
   }
   return (
     <form
-      name="fancy-contact"
+      name="fancy-contact-particulier"
       method="POST"
       data-netlify="true"
       data-netlify-honeypot="bot-field"
--- src/components/form-zakelijk.js
+++ src/components/form-zakelijk.js
@@ -1,58 +1,189 @@
-import React from 'react'
-import fancyFormStyle from './fancy-form-style.module.scss'
+import React, { useState } from "react"
+import fancyFormStyle from "./fancy-form-style.module.scss"
+import encode from "../helpers/encode"
+
+const initialFormState = {
+  name: "",
+  lastName: "",
+  companyName: "",
+  function: "",
+  sector: "",
+  email: "",
+  address: "",
+  postcode: "",
+  city: "",
+  phonenumber: "",
+  message: "",
+  gender: "Heer",
+}
 
 export default function FormZakelijk() {
-  const handleSubmit = e => {
-    console.log('you clicked submit button on zakelijk')
+  const [formState, setFormState] = useState(initialFormState)
+
+  const handleGenderChange = e => {
+    setFormState({
+      ...formState,
+      gender: e.target.value,
+    })
   }
-    return (
-        <form onSubmit={handleSubmit}>
-            <div className={fancyFormStyle.inputs}>
-              <div className={fancyFormStyle.formLeftPart}>
-                <div className={fancyFormStyle.gender} >
-                  <input type="radio" value="Heer"/>
-                  <span> {"  "}</span>
-                  <label>Heer</label>
-                  <span> {"  "}</span>
-                  <input type="radio" value="Heer"/>
-                  <span> {"  "}</span>
-                  <label>Mevrouw</label>
-                </div>
-                <input type="text" placeholder="Voornaam *" />
-                <input type="text" placeholder="Achternaam *" />
-                <input type="text" placeholder="Bedrijf *" />
-                <input type="text" placeholder="Functie" />
-                <input type="text" placeholder="Sector" />
-              </div>
-              <div className={fancyFormStyle.formRightPart}>
-              <div className={fancyFormStyle.gender} style={{visibility: 'hidden'}}>
-                  <input type="radio" />
-                  <span> {"  "}</span>
-                  <label>Heer</label>            
-                  <span> {"  "}</span>
-                  <input type="radio" />
-                  <span> {"  "}</span>
-                  <label>Mevrouw</label>
-                </div>
-                <input type="text" placeholder="E-mail *" />
-                <input type="text" placeholder="Telefoon *" />
-                <input type="text" placeholder="Adres *" />
-                <input type="text" placeholder="Postcode *" />
-                <input type="text" placeholder="Plaats *" />
-                <div className={fancyFormStyle.textAreaDiv}>
-                  <textarea
-                    style={{ width: "100%" }}
-                    rows="3"
-                    type="text"
-                    placeholder="Waar kunnen we u mee helpen?"
-                  />
-                </div>
-                {/* <input type="text" placeholder="Enter Your Answer *" /> */}
-                <div className={fancyFormStyle.buttonDiv}>
-                  <button type="submit">Verzenden</button>
-                </div>
-              </div>
-            </div>
-          </form>
-    )
+
+  const handleInputChange = e => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "fancy-contact-zakelijk", ...formState }),
+    })
+      .then(() => {
+        console.log("sent from fancy contact: ", formState)
+        alert("Successfuly sent!")
+      })
+      .catch(error => alert(error))
+
+    e.preventDefault()
+    setFormState(initialFormState)
+  }
+  return (
+    <form
+      name="fancy-contact-zakelijk"
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      onSubmit={handleSubmit}
+    >
+      <div className={fancyFormStyle.inputs}>
+        <div className={fancyFormStyle.formLeftPart}>
+          <div className={fancyFormStyle.gender}>
+            <input
+              type="radio"
+              value="Heer"
+              name="gender"
+              onChange={handleGenderChange}
+              checked={formState.gender === "Heer"}
+            />
+            <span> {"  "}</span>
+            <label>Heer</label>
+            <span> {"  "}</span>
+            <input
+              type="radio"
+              value="Mevrouw"
+              name="gender"
+              onChange={handleGenderChange}
+              checked={formState.gender === "Mevrouw"}
+            />
+            <span> {"  "}</span>
+            <label>Mevrouw</label>
+          </div>
+          <input
+            type="text"
+            placeholder="Voornaam *"
+            name="name"
+            value={formState.name}
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Achternaam *"
+            name="lastName"
+            value={formState.lastName}
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Bedrijf *"
+            name="companyName"
+            value={formState.companyName}
+            required
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Functie"
+            name="function"
+            value={formState.function}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Sector"
+            name="sector"
+            value={formState.sector}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className={fancyFormStyle.formRightPart}>
+          <div
+            className={fancyFormStyle.gender}
+            style={{ visibility: "hidden" }}
+          >
+            <input type="radio" />
+            <span> {"  "}</span>
+            <label>Heer</label>
+            <span> {"  "}</span>
+            <input type="radio" />
+            <span> {"  "}</span>
+            <label>Mevrouw</label>
+          </div>
+          <input
+            type="text"
+            placeholder="E-mail *"
+            name="email"
+            value={formState.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="number"
+            placeholder="Telefoon *"
+            name="phonenumber"
+            value={formState.phonenumber}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Adres *"
+            name="address"
+            value={formState.address}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Postcode *"
+            name="postcode"
+            value={formState.postcode}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Plaats *"
+            name="city"
+            value={formState.city}
+            onChange={handleInputChange}
+          />
+          <div className={fancyFormStyle.textAreaDiv}>
+            <textarea
+              name="message"
+              style={{ width: "100%" }}
+              rows="3"
+              type="text"
+              placeholder="Waar kunnen we u mee helpen?"
+              value={formState.message}
+              onChange={handleInputChange}
+            />
+          </div>
+          {/* <input type="text" placeholder="Enter Your Answer *" /> */}
+          <div className={fancyFormStyle.buttonDiv}>
+            <button type="submit">Verzenden</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
 }
